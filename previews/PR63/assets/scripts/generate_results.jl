9307c422f6104d3369a3c42dacf992f1b4503866
DilumAluthge <DilumAluthge@users.noreply.github.com>
2020-07-13 02:11:13 +0000

    Deploying to gh-pages from  @ bddf775b64a962fd8fd3db58753591dae6be22d5 🚀

---
+++ previews/PR63/assets/scripts/generate_results.jl
@@ -0,0 +1,25 @@
+"""
+    genplain(s)
+
+Small helper function to run some code and redirect the output (stdout) to a file.
+"""
+function genplain(s::String)
+    open(joinpath(dir, "output", "$(splitext(s)[1]).txt"), "w") do outf
+        redirect_stdout(outf) do
+            include(joinpath(dir, s))
+        end
+    end
+end
+
+const FILE = abspath(@__FILE__) # _assets/scripts/generate_results.jl
+const DIR = abspath(dirname(FILE)) # _assets/scripts/
+
+const package_list_jl_file = abspath(DIR, "package_list.jl") # _assets/scripts/package_list.jl
+include(package_list_jl_file)
+const package_list_md_file = abspath(DIR, "package_list.md") # _assets/scripts/package_list.md
+const packages_to_exclude = String[]
+const packages_to_include = String[]
+generate_package_list("JuliaHealth",
+                      package_list_md_file;
+                      packages_to_exclude = packages_to_exclude,
+                      packages_to_include = packages_to_include)
